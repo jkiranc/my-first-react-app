4afadb52b5055de29fbc66d480416c91d880155f
Kiran jc <jkiranc@gmail.com>
2019-11-12 11:24:57 +0530

    Updates

---
+++ precache-manifest.dfc3a7ffe3593f6e3c37ba0adc113c93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e9b47d52144f5b3d6816291f2462b9",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "f2bafb33a6f035627488",
+    "url": "/my-first-react-app/static/css/main.9f7f3b26.chunk.css"
+  },
+  {
+    "revision": "0ee5a65d2f450e4fafe2",
+    "url": "/my-first-react-app/static/js/2.b9f87c9d.chunk.js"
+  },
+  {
+    "revision": "f2bafb33a6f035627488",
+    "url": "/my-first-react-app/static/js/main.bbaa4ba1.chunk.js"
+  },
+  {
+    "revision": "174dbdd431cd213fcdcc",
+    "url": "/my-first-react-app/static/js/runtime-main.bce23042.js"
+  }
+]);
